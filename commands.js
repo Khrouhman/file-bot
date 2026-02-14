a6ffc2beebf8ef589f6b6f463ef1db7857b7812f
Khrouhman <acpvg@yahoo.com>
2026-02-14 15:50:28 -0800

    moved global command to utils

--- commands.js
+++ commands.js
@@ -1,19 +1,6 @@
 import 'dotenv/config';
 import { InstallGlobalCommands } from './utils.js';
 
-async function InstallGlobalCommands(appId, commands) {
-  // API endpoint to overwrite global commands
-  const endpoint = `applications/${appId}/commands`;
-
-  try {
-    // This is calling the bulk overwrite endpoint: https://discord.com/developers/docs/interactions/application-commands#bulk-overwrite-global-application-commands
-    await DiscordRequest(endpoint, { method: 'PUT', body: commands });
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-
 // Simple test command
 const TEST_COMMAND = {
   name: 'test',
